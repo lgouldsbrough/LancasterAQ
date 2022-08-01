661a55b3bb33f02f0ce88567767e80d05533453d
Lily Gouldsbrough <lilygouldsbrough@outlook.com>
2022-08-01 12:12:45 +0100

    addings tests to match updated dataobject

--- tests/test_standard.py
+++ tests/test_standard.py
@@ -1,31 +1,48 @@
+import numpy as np
+import pytest
 import LancasterAQ as laq
 
+@pytest.fixture
+def graph_object():
+    return laq.GraphObject()
 
-def test_to_numpy():
-    data = laq.GraphObject()
-    data_to_numpy = data.to_numpy()
+@pytest.fixture
+def tabular_object():
+    return laq.TabularObject()
+
+def test_dataset_graph():
+    assert laq.dataset('GraphObject')
+
+def test_dataset_tabular():
+    assert laq.dataset('TabularObject')
+
+class TestGraphObject:
+    def test_graph_object(self, graph_object):
+        assert graph_object
+
+    def test_to_numpy(self, graph_object):
+        data_to_numpy = graph_object.to_numpy()
+        assert isinstance(data_to_numpy, np.ndarray)
+
+    def test_to_dict(self, graph_object):
+        data_to_dict = graph_object.to_dict()
+
+    def test_to_edgelist(self, graph_object):
+        data_to_edgelist = graph_object.to_edgelist()
+
+    def test_to_dict_of_lists(self, graph_object):
+        data_to_dict_of_lists = graph_object.to_dict_of_lists()
+
+    def test_to_scipy(self, graph_object):
+        data_to_scipy = graph_object.to_scipy()
+
+    def test_to_json(self, graph_object):
+        data_to_json = graph_object.to_json()
 
 
-def test_to_dict():
-    data = laq.GraphObject()
-    data_to_dict = data.to_dict()
+class TestTabularObject:
+    def test_tabular_object(self, tabular_object):
+        assert tabular_object
 
-
-def test_to_edgelist():
-    data = laq.GraphObject()
-    data_to_edgelist = data.to_edgelist()
-
-
-def test_to_dict_of_lists():
-    data = laq.GraphObject()
-    data_to_dict_of_lists = data.to_dict_of_lists()
-
-
-def test_to_scipy():
-    data = laq.GraphObject()
-    data_to_scipy = data.to_scipy()
-
-
-def test_to_json():
-    data = laq.GraphObject()
-    data_to_json = data.to_json()
+    def test_to_numpy(self, tabular_object):
+        assert isinstance(tabular_object.to_numpy(), np.ndarray)
